d956cff3f713432c384ac5700e4b1713eccb84c2
holyoke <jasonebueng@gmail.com>
2016-10-22 13:49:14 -0700

    completes primality

--- primality/lib/main.rb
+++ primality/lib/main.rb
@@ -1,2 +1,11 @@
+require 'set'
 def is_prime?(num)
+  return 'Not prime' if num <= 1
+  upper_bound = Math.sqrt(num)
+
+  (2..upper_bound).each do |i|
+    return 'Not prime' if num % i == 0
+  end
+
+  'Prime'
 end
--- primality/spec/main_spec.rb
+++ primality/spec/main_spec.rb
@@ -4,5 +4,14 @@ require 'byebug'
 describe "Primality" do
   it "does sample input" do
     expect(is_prime? 12 ).to eq 'Not prime'
+    expect(is_prime? 5 ).to eq 'Prime'
+    expect(is_prime? 7 ).to eq 'Prime'
+  end
+
+  it "handles edge cases" do
+    expect(is_prime? 1 ).to eq 'Not prime'
+    expect(is_prime? 2 ).to eq 'Prime'
+    expect(is_prime? 3 ).to eq 'Prime'
+    expect(is_prime? 20 ).to eq 'Not prime'
   end
 end
