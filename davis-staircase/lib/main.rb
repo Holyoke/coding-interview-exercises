5464dd20424117b7ada75794550cf8e779612cc7
holyoke <jasonebueng@gmail.com>
2016-10-24 13:42:23 -0700

    WIP count paths

--- davis-staircase/lib/main.rb
+++ davis-staircase/lib/main.rb
@@ -1,3 +1,15 @@
 def count_paths(steps)
-  
+  paths = Array.new
+
+  byebug if steps > 1
+
+  if steps < 0
+    return 0
+  elsif steps == 0
+    return 1
+  elsif paths[steps].nil? == false
+    return paths[steps]
+  else
+    return  paths[steps] = count_paths(steps - 1) + count_paths(steps - 2) + count_paths(steps - 3)
+  end
 end
--- davis-staircase/spec/main_spec.rb
+++ davis-staircase/spec/main_spec.rb
@@ -7,4 +7,14 @@ describe "Davis Staircase" do
     expect(count_paths 3).to eq 4
     expect(count_paths 7).to eq 44
   end
+
+  it "handles basic cases" do
+    expect(count_paths 1).to eq 1
+    expect(count_paths 2).to eq 2
+    expect(count_paths 3).to eq 4
+  end
+
+  it "handles edge cases" do
+    expect(count_paths 27).to eq 8646064
+  end
 end
