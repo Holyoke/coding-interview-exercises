fc2f93abebf85549a1b533517d00df3e28657f79
holyoke <jasonebueng@gmail.com>
2016-10-27 13:19:11 -0700

    implement a dp version of fibs

--- fibonacci/lib/main.rb
+++ fibonacci/lib/main.rb
@@ -1,6 +1,8 @@
-def fibonacci(num, memo = [0, 1, 1])
-  if memo[num].nil?
-    memo[num] = fibonacci(num - 1, memo) + fibonacci(num - 2, memo)
+def fibonacci(num)
+  memo = [0,1,1]
+
+  (3..num).each do |step|
+    memo[step] = memo[step - 1] + memo[step - 2]
   end
 
   memo[num]
@@ -14,4 +16,12 @@ def fibonacci_rec(num)
   else
     return fibonacci(num - 1) + fibonacci(num - 2)
   end
+end
+
+def fibonacci_memo(num, memo = [0, 1, 1])
+  if memo[num].nil?
+    memo[num] = fibonacci(num - 1, memo) + fibonacci(num - 2, memo)
+  end
+
+  memo[num]
 end
