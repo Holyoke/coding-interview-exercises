a6e430ededdd1ca2d3f1a6d9e12ad838627be310
holyoke <jasonebueng@gmail.com>
2016-10-27 15:00:17 -0700

    implements dp version of count paths

--- davis-staircase/lib/main.rb
+++ davis-staircase/lib/main.rb
@@ -1,23 +1,35 @@
-def count_paths_memo(steps, memo = Array.new)
-  if steps < 0
-    return 0
-  elsif steps == 0
-    return 1
+def count_paths(level)
+  return 0 if level < 0
+
+  paths = [1,1,2]
+
+  (3..level).each do |lvl|
+      paths[lvl] = paths[lvl - 1] + paths[lvl - 2] + paths[lvl - 3]
   end
 
-  if memo[steps].nil?
-    memo[steps] = count_paths(steps - 1, memo) + count_paths(steps - 2, memo) + count_paths(steps - 3, memo)
-  end
-
-  memo[steps]
+  paths[level]
 end
 
-def count_paths_rec(steps)
-  if steps < 0
+def count_paths_rec(levels)
+  if levels < 0
     return 0
-  elsif steps == 0
+  elsif levels == 0
     return 1
   else
-    return count_paths(steps - 1) + count_paths(steps - 2) + count_paths(steps - 3)
+    return count_paths(levels - 1) + count_paths(levels - 2) + count_paths(levels - 3)
   end
+end
+
+def count_paths_memo(levels, memo = Array.new)
+  if levels < 0
+    return 0
+  elsif levels == 0
+    return 1
+  end
+
+  if memo[levels].nil?
+    memo[levels] = count_paths(levels - 1, memo) + count_paths(levels - 2, memo) + count_paths(levels - 3, memo)
+  end
+
+  memo[levels]
 end
