7f740f4e1a5ffd7bdf42394d99d16525eceb023a
holyoke <jasonebueng@gmail.com>
2016-10-27 13:07:47 -0700

    adds memo fibonacci version

--- fibonacci/lib/main.rb
+++ fibonacci/lib/main.rb
@@ -1,4 +1,12 @@
-def fibonacci(num)
+def fibonacci(num, memo = [0, 1, 1])
+  if memo[num].nil?
+    memo[num] = fibonacci(num - 1, memo) + fibonacci(num - 2, memo)
+  end
+
+  memo[num]
+end
+
+def fibonacci_rec(num)
   if num <= 1
     return 1
   elsif num == 2
