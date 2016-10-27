544a4278ca470c2bdd26b6f767fea8b2be5c1d17
holyoke <jasonebueng@gmail.com>
2016-10-27 13:30:31 -0700

    adds memory efficient dp version of fibs

--- fibonacci/lib/main.rb
+++ fibonacci/lib/main.rb
@@ -2,10 +2,13 @@ def fibonacci(num)
   memo = [0,1,1]
 
   (3..num).each do |step|
-    memo[step] = memo[step - 1] + memo[step - 2]
+    current_fib = memo[1] + memo[2]
+    memo[0] = memo[1]
+    memo[1] = memo[2]
+    memo[2] = current_fib
   end
 
-  memo[num]
+  memo[2]
 end
 
 def fibonacci_rec(num)
@@ -21,6 +24,16 @@ end
 def fibonacci_memo(num, memo = [0, 1, 1])
   if memo[num].nil?
     memo[num] = fibonacci(num - 1, memo) + fibonacci(num - 2, memo)
+  end
+
+  memo[num]
+end
+
+def fibonacci_dp(num)
+  memo = [0,1,1]
+
+  (3..num).each do |step|
+    memo[step] = memo[step - 1] + memo[step - 2]
   end
 
   memo[num]
