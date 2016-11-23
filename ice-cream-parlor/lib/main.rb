63710c71802e9c39407fd5db482d7304635af754
holyoke <jasonebueng@gmail.com>
2016-11-22 16:40:11 -0800

    passes sample tests

--- ice-cream-parlor/lib/main.rb
+++ ice-cream-parlor/lib/main.rb
@@ -1,3 +1,10 @@
-def find_flavors(total_money, num_flavors, cost_map = [])
+def find_flavors(total_money, costs)
+  cost_map = costs.each_with_index.inject(Hash.new) { |sum, (i,j)| sum[i] = j + 1; sum }
 
+  costs.each_with_index do |cost, idx|
+    complement_cost = total_money - cost
+    return "#{idx + 1 } #{cost_map[complement_cost]}" if cost_map[complement_cost]
+  end
+
+  false
 end
